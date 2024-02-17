e61ea6c662fd6fcf9ce667a9d2316f80ad80f9dd
Dominik Zivko <domy.zivko@gmail.com>
2024-02-17 18:43:16 +0100

    Add notes CRUD api

--- note-app-server/src/app.ts
+++ note-app-server/src/app.ts
@@ -1,13 +1,28 @@
 import { Application, Request, Response, Router } from "express"
+import { Note } from "./models/note"
+import noteController from "./controllers/noteController"
 
 const express = require('express')
 const app: Application = express()
 const api: Router = express.Router()
 
-api.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+api.get("/notes", (req: Request, res: Response) => {
+    res.send(noteController.getNotes())
 })
 
+api.post("/notes", (req: Request, res: Response) => {
+    res.send(noteController.createNote(req.body.title, req.body.content))
+})
+
+api.patch("/notes/:id", (req: Request, res: Response) => {
+    res.send(noteController.updateNote(req.params.id, req.body))
+})
+
+api.delete("/notes/:id", (req: Request, res: Response) => {
+    res.send(noteController.deleteNote(req.params.id))
+})
+
+app.use(express.json());
 app.use("/api", api)
 
-export = app
+export default app
---
+++ note-app-server/src/controllers/noteController.ts
@@ -0,0 +1,45 @@
+import { Note } from "../models/note";
+import { timestampSeconds } from "../utils/dateUtils";
+
+class NoteController {
+    private notes: Note[] = []
+
+    getNotes(): Note[] {
+        return this.notes
+    }
+
+    createNote(title: string, content: string): Note {
+        const newNote: Note = {
+            id: crypto.randomUUID(),
+            title: title,
+            content: content,
+            createdAt: timestampSeconds(),
+            updatedAt: null
+        }
+        this.notes.push(newNote)
+        return newNote
+    }
+
+    updateNote(id: string, newNote: Partial<Note>): Note {
+        const existingNote = this.notes.find(note => note.id === id)
+        if (!existingNote) throw new Error("Note not found")
+
+        const updatedNote = {
+            ...existingNote,
+            ...newNote,
+            updatedAt: timestampSeconds()
+        }
+
+        this.notes = this.notes.map((note) =>
+            updatedNote.id === id ? updatedNote : note,
+        )
+
+        return updatedNote
+    }
+
+    deleteNote(id: string) {
+        this.notes = this.notes.filter(note => note.id !== id)
+    }
+}
+
+export default new NoteController()
